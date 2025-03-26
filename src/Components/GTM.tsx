3f2a47ac992cd7576c0eb550d60747a89efdb126
amedeirosg <gomesandremedeiros@gmail.com>
2025-03-26 16:44:27 -0300

    add gtm

---
+++ src/Components/GTM.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+//@ts-ignore
+import TagManager from "react-gtm-module";
+
+const GTM_ID = "GTM-KKVCZ8CG"; // Substitua pelo seu ID do GTM
+
+export default function GTM() {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      TagManager.initialize({ gtmId: GTM_ID });
+    }
+  }, []);
+
+  return null; // O componente não precisa renderizar nada
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import GTM from "@/Components/GTM";
 
 export const metadata: Metadata = {
   title: "Rany Engenharia | Soluções em Projetos",
@@ -13,7 +14,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head></head>
+      <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KKVCZ8CG"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        <GTM />
+        {children}
+      </body>
     </html>
   );
 }
